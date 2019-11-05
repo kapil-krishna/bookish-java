3ac0f66d67e477dbc1d5954a4b53b6aa503794fe
Kapil Krishna <kapil.krishna.yo@gmail.com>
2019-11-05 11:22:08 +0000

    JDBI connection working

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,8 +1,10 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
 import java.sql.*;
+import java.util.List;
 
 import static java.lang.System.getenv;
 
@@ -37,9 +39,10 @@ public class Main {
                 String title = rs.getString("title");
                 int noOfCopies = rs.getInt("noOfCopies");
                 int noAvailable = rs.getInt("noAvailable");
-                System.out.println(id + " " + title + " " + noOfCopies + " " + noAvailable);
+                System.out.println(String.format("|%-5d|%-30s|%-5d|%-5d|",
+                        id, title, noOfCopies, noAvailable));
             }
-        } catch (SQLException e ) {
+        } catch (SQLException e) {
             System.out.println(e);
         } finally {
             if (stmt != null) {
@@ -56,8 +59,13 @@ public class Main {
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
-
-
-
+        List<Book> books = jdbi.withHandle(handle ->
+                handle.createQuery("SELECT * FROM Book")
+                        .mapToBean(Book.class)
+                        .list());
+        for (Book book : books) {
+            System.out.println(String.format("|%-5d|%-30s|%-5d|%-5d|",
+                    book.getId(), book.getTitle(), book.getNoOfCopies(), book.getNoAvailable()));
+        }
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -1,5 +1,42 @@
 package org.softwire.training.bookish.models.database;
 
 public class Book {
+    private Integer id;
+    private String title;
+    private Integer noOfCopies;
+    private Integer noAvailable;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getNoOfCopies() {
+        return noOfCopies;
+    }
+
+    public void setNoOfCopies(Integer noOfCopies) {
+        this.noOfCopies = noOfCopies;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNoAvailable() {
+        return noAvailable;
+    }
+
+    public void setNoAvailable(Integer noAvailable) {
+        this.noAvailable = noAvailable;
+    }
 }
+
+
